454da955a3449cb0125261c1a57459bb479bd673
Nikhil Limbu <nikhillimbu918@gmail.com>
2026-07-24 12:07:08 +0545

    feat: add new UI components (Separator, Sheet, Sidebar, Skeleton, Slider, and more)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { fontMono, fontSans } from "@/lib/fonts";
 import "./globals.css";
+import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,7 +17,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${fontSans.variable} ${fontMono.variable} h-full antialiased`}>
+    <html
+      lang="en"
+      className={cn(
+        "h-full",
+        "antialiased",
+        fontSans.variable,
+        fontMono.variable,
+        "font-sans",
+        inter.variable,
+      )}
+    >
       <body className="flex min-h-full flex-col">{children}</body>
     </html>
   );
